5e721aa0f7c898ffbdac5aefea834612cb15765c
Olamilekan <78114886+Element27@users.noreply.github.com>
2023-01-30 10:06:57 +0100

    hero bug fixed

--- components/heroTop.tsx
+++ components/heroTop.tsx
@@ -6,7 +6,7 @@ const HeroTop = () => {
   return (
     <div className="bg-first-bg w-full h-screen bg-cover bg-no-repeat bg-center">
       <div className="w-full h-full bg-black/80 flex justify-center items-center gap-8 text-gray-300 flex-col lg:flex-row px-4">
-        <div className="w-fit mx-auto">
+        <div className="w-fit lg:w-auto mx-auto lg:mx-0">
           {/* // nmae and what i do go here */}
           <h2 className="text-5xl font-bold md:text-7xl">
             Daniel <span className="text-[#dc3956]">Akinbo</span>
@@ -15,7 +15,7 @@ const HeroTop = () => {
             Frontend Developer, Technical Writer, Graphics Designer
           </h3>
         </div>
-        <div className="relative overflow-hidden rounded-full">
+        <div className="overflow-hidden rounded-full">
           {/* dp image goes here */}
           <Image src={d_tp} alt="" className="w-52 h-52 md:w-64 md:h-64" />
         </div>
